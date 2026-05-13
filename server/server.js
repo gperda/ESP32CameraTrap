4bb1c95dff2150b6c5b402c1a0d15410994952a9
Giulio Perda <giulioperda@gmail.com>
2026-05-13 10:37:07 +0200

    Fixed camId bug

--- server/server.js
+++ server/server.js
@@ -141,9 +141,9 @@ wss.on('connection', (ws, req) => {
         espClients.set(camId, ws);
         console.log(`  ESP registered: ${camId}  (total ${espClients.size})`);
         if (OTAPending) {
-          if(camId === 1)
+          if(camId === "cam1")
             ws.send('master_ota_update');
-          else if (camId === 2)
+          else if (camId === "cam2")
             ws.send('slave_ota_update')
           OTApending = false;
           console.log(`  OTA update command sent to ${camId}`);
